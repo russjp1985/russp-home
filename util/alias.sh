e66124e1431c218c8d5f15f71c330b4813359536
Russ Posluszny <russjp1985@gmail.com>
2011-12-04 01:48:25 -0500

    Add cim (create and vim) function

--- util/alias.sh
+++ util/alias.sh
@@ -3,3 +3,8 @@ alias ll="ls -l"
 alias st="svn status  | grep \"[MACDX?]\" | sort"
 alias vim="open -a /Applications/MacVim.app"
 alias yui="java -jar ~/util/tools/yui-compressor.jar"
+
+function cim() {
+  touch $1;
+  vim $1
+}
