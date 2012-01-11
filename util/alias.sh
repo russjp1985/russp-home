d21253a18133eb1d5888181a6b73e1d398c350f6
Russ Posluszny <russjp1985@gmail.com>
2012-01-11 09:24:52 -0500

    Added `here` and `there` aliases

--- util/alias.sh
+++ util/alias.sh
@@ -3,6 +3,9 @@ alias ll="ls -l"
 alias st="svn status  | grep \"[MACDX?]\" | sort"
 alias vim="open -a /Applications/MacVim.app"
 alias yui="java -jar ~/util/tools/yui-compressor.jar"
+alias here="pwd | pbcopy"
+alias there="cd \`pbpaste\`"
+alias nodeytool="~/Checkouts/nodey-tools/bin"
 
 function cim() {
   touch $1;
