81b4af0647b9e11d8bed86b4a1bdb813e7cb67bd
rposluszny <rposluszny@etsy.com>
2012-03-27 00:45:37 +0000

    Modified aliases

--- util/alias.sh
+++ util/alias.sh
@@ -7,11 +7,23 @@ alias here="pwd | pbcopy"
 alias there="cd \`pbpaste\`"
 alias nodeytool="~/Checkouts/nodey-tools/bin"
 alias git="env GIT_SSL_NO_VERIFY=true git"
+alias lc="clear; ls --width=1"
+function mini() {
+  zsh
+  RPROMPT=''
+  PROMPT='$ '
+  clear
+}
+
 alias lwd=echo `pwd` > ~/.lwd
 
 function cim() {
   touch $1;
   vim $1
+}
+
+function gitview() {
+  URL_BASE=git remote -v | grep origin | grep fetch | awk '{print $2}' | sed s/git\:/https:/ | sed s/\.git$//
 }
 
 function cd() {
