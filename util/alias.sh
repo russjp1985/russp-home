406c6561f26f5619f9cc5c74c9d513bdf78f7331
Russ P <russ@russposluszny.com>
2012-05-29 10:39:32 -0400

    Tweak mvim alias

--- util/alias.sh
+++ util/alias.sh
@@ -10,7 +10,7 @@ alias lc="clear; ls --width=1"
 alias ec="edit-class"
 alias sha1sum="shasum"
 
-alias mvim="open /Applications/MacVim.app"
+alias mvim="open /Applications/MacVim.app ."
 
 function trash() {
     mv $* ~/.Trash
